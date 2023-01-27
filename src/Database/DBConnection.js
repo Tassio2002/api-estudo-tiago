b713d58ba3c8da14ef23896737e27907b3918ce0
Tassio <81255475+Tassio2002@users.noreply.github.com>
2023-01-27 20:48:39 -0300

    Basic structure of project

---
+++ src/Database/DBConnection.js
@@ -0,0 +1,18 @@
+const pg = require('pg');
+
+let connString = "postgres://txluvqcy:1bS-72MdK2n4NUn2YHDQTS-Ss0TQT32J@babar.db.elephantsql.com/txluvqcy"
+let client = new pg.Client(connString)
+
+client.connect(function(err){
+    if(err){
+        return console.error('could not connect to postgres', err)
+    }
+    client.query('SELECT NOW() AS "theTime"', function(err, result) {
+        if(err) {
+        return console.error('error running query', err);
+        }
+        console.log(result.rows[0].theTime);
+    })
+})
+
+module.exports = client
---
+++ src/index.js
@@ -0,0 +1,98 @@
+const express = require('express')
+const app = express()
+const pool = require('./Database/DBConnection')
+const jwt = require('jsonwebtoken');
+require("dotenv-safe").config();
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+app.get('/' , async (req, res) => {
+    res.status(200).send('App rodando')
+})
+
+app.get('/getusers', async (req, res) => {
+    try {
+        const { rows } = await pool.query('SELECT * FROM users')
+        return res.status(200).send(rows)
+    } catch (err) {
+        return res.status(400).send(err)
+    }
+})
+
+app.post('/signup', async (req, res) => {
+    const { user_name, user_email, user_type, password } = req.body
+    
+    try {
+        const user = await pool.query('SELECT * FROM users WHERE user_email = ($1)', [user_email])
+        if (!user.rows[0]) {
+            const createUser = await pool.query('INSERT INTO users (user_name, user_type, user_email, password) VALUES ($1, $2, $3, $4) RETURNING *', [user_name, user_type, user_email, password])
+            return res.status(201).send(createUser.rows)
+        }else {
+            return res.status(400).send({
+                message: 'A user with this email already exists in the system, please enter your email and password'
+            })
+        }
+        
+    } catch (err) {
+        return res.status(500).send(err)
+    }
+})
+
+app.post('/login', async (req, res) => {
+    const { user_id, user_email, password } = req.body
+    try {
+        const loginData = await pool.query('SELECT user_email, password FROM users WHERE user_email LIKE ($1) AND password LIKE ($2)',
+        [user_email, password])
+        if (!loginData.rows[0]) {
+            return res.status(401).send({message: 'Invalid login, incorrect email or password, please try again'})
+        } else {
+            const token = jwt.sign({ user_id }, 'secret', {
+                expiresIn: 100
+            })
+            return res.json({ auth: true, token: token, message: "user successfuly logged in" })
+        }
+    } catch (err) {
+        return res.status(500).send(err)
+    }
+})
+
+app.patch('/user/:user_id', async (req, res) => {
+    const { user_id } = req.params
+    const { user_name, user_type, password } = req.body
+
+    try {
+        const updateUser = await pool.query('UPDATE users SET user_name = ($1), user_type = ($2), password = ($3) WHERE user_id = ($4) RETURNING *', 
+            [user_name, user_type, password, user_id])
+        return res.status(200).send(updateUser.rows)
+    } catch (error) {
+        res.status(400).send('bad request')
+    }
+})
+
+app.delete('/user/:user_id', verifyJWT, async (req, res) => {
+    const { user_id } = req.params
+    try {
+        await pool.query('DELETE FROM users WHERE user_id = ($1) RETURNING *', [user_id])
+        return res.status(200).send({
+            message: 'User successfully deleted',
+        })
+    } catch (err) {
+        return res.status(400).send(err)
+    }
+})
+
+function verifyJWT(req, res, next) {
+    const token = req.headers['x-access-token']
+
+    if (!token) return res.status(401).json({ auth: false, message: 'No token provided.' })
+
+    jwt.verify(token, 'secret', function (err, decoded) {
+        if (err) return res.status(401).json({ auth: false, message: 'Failed to authenticate token' })
+        req.user_id = decoded.id
+        next()
+    })
+}
+
+app.listen(3333)
+console.log(`Server running or port 3333`)
